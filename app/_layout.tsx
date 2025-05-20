3fba1b5f695a1aa198e16ec49428737f31a96c66
E1622 <nuno.carneiro@itsector.pt>
2025-05-20 12:07:28 +0100

    add biometric authentication

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,54 @@
 import Tabs from "@/navigation/tabs";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import * as LocalAuthentication from "expo-local-authentication";
+import { useState } from "react";
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import './globals.css';
 
 const queryClient = new QueryClient();
 
 export default function RootLayout() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <SafeAreaProvider>
-        <Tabs />
-      </SafeAreaProvider>
-    </QueryClientProvider>
-  );
+  const [authenticated, setAuthenticated] = useState(false);
+  const [hasBiometrics, setHasBiometrics] = useState(true);
+
+  const checkDeviceForHardware = async () => {
+      let compatible = await LocalAuthentication.hasHardwareAsync();
+      if(compatible) {
+        checkForBiometrics();
+      } else {
+        setHasBiometrics(false);
+      }
+  };
+
+  const checkForBiometrics = async () => {
+      let biometricRecords = await LocalAuthentication.isEnrolledAsync();
+      if(biometricRecords) {
+        authenticate();
+      } else {
+        setHasBiometrics(false);
+      }
+    };
+
+  const authenticate = async () => {
+      let result = await LocalAuthentication.authenticateAsync();
+      if (result.success) {
+        setAuthenticated(true);
+      } else if (result.error == "user_cancel") {
+        authenticate();
+      }
+  }
+    
+  if(!authenticated && hasBiometrics) {
+    checkDeviceForHardware();
+  }
+
+  if(authenticated || !hasBiometrics) {
+    return (
+      <QueryClientProvider client={queryClient}>
+        <SafeAreaProvider>
+          <Tabs />
+        </SafeAreaProvider>
+      </QueryClientProvider>
+    );
+  }
 }
